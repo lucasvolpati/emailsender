1fe0ce9eac14878de9414d4f95d9f3319e3820b5
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2022-05-13 18:16:44 -0300

    update to version 2

--- test/index.php
+++ test/index.php
@@ -1,24 +1,21 @@
 <?php
 require __DIR__ ."/../vendor/autoload.php";
 
-use Email\Email\Email;
+use MasterCode\Email\Email;
 
 $mail = new Email(
-    "smptp.domain.com.br", //Servidor de envio smtp.
-    "email@domain.com.br", //Email usuário.
-    "pass", //Senha do email.
-    "tls", // Assume "ssl" ou "tsl".
-    "587", //Porta, assume 465 ou 587, verificar.
-    "email.sender@domain.com", //Email que fará o envio.
-    "My Name", //Nome do remetente.
-    2 //Smtp Debug, o padrão é 2 .
+    "smtps.domain.com",
+    "sender@domain.com",
+    "12345678",
+    "sender@domain.com",
+    "My Name"
 );
 
 $mail->sendEmail(
-    $address, //Quem recebe o email
-    $addressName, //Nome de quem recebe
-    $subject, //Assunto do email.
-    $body, //Conteudo do email, mensagem, etc.
-    $replyMail, //Email do cliente que fez contato, para onde será respondido.
-    $replyName //Nome do contato.
+    "receiver@domain.com",
+    "Receiver Name",
+    "Contact By Site",
+    "Here is the formulary message by your site.",
+    "client@email.com", //get from formulary
+    "Client Name"
 );
